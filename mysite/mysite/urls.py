34cef08e637ef34cdde5b72c9e89058dcb9ffd85
Pando <ndjman7@gmail.com>
2016-11-04 16:58:33 +0900

    Add root urls member

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('task.urls', namespace='task')),
+    url(r'^member/', include('member.urls', namespace='member')),
 ]
