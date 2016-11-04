23960507ece3a4b8dd317e47e6a0231adeadd396
Pando <ndjman7@gmail.com>
2016-11-04 13:11:44 +0900

    Add calendar function

--- mysite/task/views.py
+++ mysite/task/views.py
@@ -1,5 +1,13 @@
+import calendar
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'common/index.html', {})
+
+
+def task_calendar(request):
+    c = calendar.Calendar(calendar.SUNDAY).yeardays2calendar(2016, 1)
+    return render(request, 'task/calendar.html', {'year': c})
+
+
