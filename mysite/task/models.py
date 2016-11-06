632225d5e7dcef532855cc29dcda7c1d1566e771
Pando <ndjman7@gmail.com>
2016-11-06 22:09:32 +0900

    Change Model Task -> ToDoList Add Model Task

--- mysite/task/models.py
+++ mysite/task/models.py
@@ -2,18 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Task(models.Model):
+class ToDoList(models.Model):
     user = models.ForeignKey(User)
-    task1 = models.CharField(max_length=50, blank=True, null=True)
-    task2 = models.CharField(max_length=50, blank=True, null=True)
-    task3 = models.CharField(max_length=50, blank=True, null=True)
-    task4 = models.CharField(max_length=50, blank=True, null=True)
-    task5 = models.CharField(max_length=50, blank=True, null=True)
-    task6 = models.CharField(max_length=50, blank=True, null=True)
-    task7 = models.CharField(max_length=50, blank=True, null=True)
-    task8 = models.CharField(max_length=50, blank=True, null=True)
-    task9 = models.CharField(max_length=50, blank=True, null=True)
-    task10 = models.CharField(max_length=50, blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     date = models.DateField(auto_now_add=True)
@@ -24,17 +14,13 @@ class Task(models.Model):
     def __str__(self):
         return str(self.date)
 
-    @property
-    def tasks(self):
-        tasks = []
-        tasks.append(self.task1)
-        tasks.append(self.task2)
-        tasks.append(self.task3)
-        tasks.append(self.task4)
-        tasks.append(self.task5)
-        tasks.append(self.task6)
-        tasks.append(self.task7)
-        tasks.append(self.task8)
-        tasks.append(self.task9)
-        tasks.append(self.task10)
-        return tasks
+
+class Task(models.Model):
+    mission = models.ForeignKey(ToDoList)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
