a55383d62e6fde1401d503e2f2f9e0bbb47744eb
ndjman7 <ndjman7@gmail.com>
2017-02-26 17:48:14 +0900

    Add Goal model

--- mysite/task/models.py
+++ mysite/task/models.py
@@ -85,3 +85,14 @@ class Task(models.Model):
                 change_task.ranking -= 1
                 change_task.save()
 
+
+class Goal(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
