9249eabb350abc8c8c96f57e94714445d6815bcf
ndjman7 <ndjman7@gmail.com>
2017-02-28 00:06:41 +0900

    diary create view 구현

--- mysite/diary/views.py
+++ mysite/diary/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import FutureDiary, RealDiary
+
+
+def future_diary_create(request):
+    if request.method == 'POST':
+        FutureDiary.objects.create(user=request.user)
+        return redirect('task:task_calendar')
+
+
+def future_diary_edit(request):
+    pass
+
+
+def real_diary_create(request):
+    if request.method == 'POST':
+        RealDiary.objects.get_or_create(user=request.user)
+        return redirect('task:task_calendar')
+
+
+def real_diary_edit(request):
+    pass
